c4fef68ac34a603beced841a32c4464db1ec4b89
AlamMorium7 <110713699+AlamMorium7@users.noreply.github.com>
2022-10-22 17:41:34 +0600

    Add files via upload

---
+++ helloworld.asm
@@ -0,0 +1,27 @@
+.MODEL SMALL ;  
+
+.STACK 100H  
+
+.DATA   ; this is where we keep all data variables
+MYMSG DB 'HELLO WORLD$'  ; variable
+
+.CODE   ; this is where we keep all codes
+ MAIN PROC 
+     
+    MOV AX,@DATA   ;name of data segment define in .DATA
+    MOV DS,AX 
+    
+    
+    
+    ;now show the hello world message
+    MOV AH, 9
+    LEA DX, MYMSG
+    INT 21H ; call DOS to Print
+    
+    ;exit to the disk operating system (DOS)
+    MOV AX,4CH  ;AX=4CH
+    INT 21H   ; calling DOS for help
+MAIN ENDP  ;similar to the end of main function in C  
+    
+        
+ END MAIN  ; the end of the whole program
