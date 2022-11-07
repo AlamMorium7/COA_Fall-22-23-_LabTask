3da4760838f44d4d3186b75e818c05c4a2383124
AlamMorium7 <110713699+AlamMorium7@users.noreply.github.com>
2022-11-07 19:34:26 +0600

    Create F_Lab_ii.asm

---
+++ F_Lab_ii.asm
@@ -0,0 +1,6 @@
+;Write a program to display a "?", read two capital letters, and display them on the next line In alphabetical order. Hints: use cmp, jg, xchg
+
+;Sample Output
+;Enter character: CB 
+;BC
+;Thank you. 
